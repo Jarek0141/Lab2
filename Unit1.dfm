82ead08a79c7e960081571d950af222b5a078824
Иванюков Игорь Вячеславович <jarek.list@mail.ru>
2016-03-02 22:39:02 +0700

    1 version
    Exel

---
+++ Unit1.dfm
@@ -0,0 +1,74 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'lab2'
+  ClientHeight = 210
+  ClientWidth = 197
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 24
+    Top = 24
+    Width = 85
+    Height = 13
+    Caption = #1043#1088#1072#1092#1080#1082' '#1092#1091#1085#1082#1094#1080#1080
+  end
+  object Label2: TLabel
+    Left = 24
+    Top = 56
+    Width = 12
+    Height = 13
+    Caption = 'Xn'
+  end
+  object Label3: TLabel
+    Left = 24
+    Top = 91
+    Width = 11
+    Height = 13
+    Caption = 'Xk'
+  end
+  object Label4: TLabel
+    Left = 24
+    Top = 126
+    Width = 21
+    Height = 13
+    Caption = #1064#1072#1075
+  end
+  object Edit1: TEdit
+    Left = 61
+    Top = 53
+    Width = 75
+    Height = 21
+    TabOrder = 0
+  end
+  object Edit2: TEdit
+    Left = 61
+    Top = 88
+    Width = 75
+    Height = 21
+    TabOrder = 1
+  end
+  object Edit3: TEdit
+    Left = 61
+    Top = 123
+    Width = 75
+    Height = 21
+    TabOrder = 2
+  end
+  object Button1: TButton
+    Left = 61
+    Top = 150
+    Width = 75
+    Height = 25
+    Caption = #1055#1091#1089#1082
+    TabOrder = 3
+    OnClick = Button1Click
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,119 @@
+unit Unit1;
+
+interface
+
+uses
+Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, ComObj, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Excel_TLB, VBIDE_TLB,
+  Math, Graph_TLB;
+
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+   Form1: TForm1;
+  AChart: _Chart;
+  mchart: ExcelChart;
+  mshape: Shape;
+  CellName: String;
+  oChart: ExcelChart;
+  Col: Char;
+  defaultLCID: Cardinal;
+  Row: Integer;
+  mAxis:Axis;
+  GridPrevFile: string;
+  MyDisp: IDispatch;
+  ExcelApp: ExcelApplication;
+  v:variant;
+  Sheet: ExcelWorksheet;
+  y1, y2, y3, x, xb, xe, st: Extended;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+ ExcelApp := CreateOleObject('Excel.Application') as ExcelApplication;
+
+  ExcelApp.Visible[0] := True;
+
+  ExcelApp.Workbooks.Add(xlWBatWorkSheet, 0);
+
+  Sheet := ExcelApp.Workbooks[1].WorkSheets[1] as ExcelWorksheet;
+
+  ExcelApp.Application.ReferenceStyle[0] := xlA1;
+
+
+  xb:=StrToFloat(Edit1.Text);
+  xe:=StrToFloat(Edit2.Text);
+  st:=StrToFloat(Edit3.Text);
+
+
+
+  col:='A';
+  x:=xb;
+  Sheet.Range[col+'1', col+'1'].Value[xlRangeValueDefault]:='x';
+  row:=2;
+  while (x<=xe) and (x>=xb) do
+    begin
+      Sheet.Range[col+IntToStr(row), col+IntToStr(row)].Value[xlRangeValueDefault]:=x;
+      x:=x+st;
+      row:=row+1;
+    end;
+
+  col:='B';
+  x:=xb;
+  Sheet.Range[col+'1', col+'1'].Value[xlRangeValueDefault]:='y1';
+  row:=2;
+  while (x<=xe) and (x>=xb) do
+    begin
+    if x<=-1 then y1:=power(x,3);
+    if (x<0)and(x>-1) then y1:=power(3,x);
+    if x>=0 then y1:=x+3;
+      Sheet.Range[col+IntToStr(row), col+IntToStr(row)].Value[xlRangeValueDefault]:=y1;
+      x:=x+st;
+      row:=row+1;
+    end;
+
+sheet.Range['A2','B'+inttostr(row)].Select;
+mshape:=Sheet.Shapes.AddChart(xlXYScatterSmoothNoMarkers,250,1,800,800);
+mchart:=(mshape.Chart as ExcelChart).Location(xlLocationAsNewSheet,EmptyParam);
+ExcelApp.Application.ActiveWorkbook.ActiveChart.SetElement(1);
+ExcelApp.Application.ActiveWorkbook.ActiveChart.ChartTitle[0].Text:='График функции';
+MyDisp:=mchart.Axes(xlValue, xlPrimary, 0);
+
+   mAxis:=Axis(MyDisp);
+
+    mAxis.HasTitle:=True;
+    mAxis.AxisTitle.Caption:='х';
+
+MyDisp:=mchart.Axes(xlCategory, xlPrimary, 0);
+
+   mAxis:=Axis(MyDisp);
+
+    mAxis.HasTitle:=True;
+    mAxis.AxisTitle.Caption:='Y';
+
+ExcelApp.Application.ActiveWorkbook.ActiveChart.SetElement(328);
+
+
+end;
+
+end.
